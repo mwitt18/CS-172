f58591ed1c69fc3bfc39f3b3adf02709f87e1275
Marika Witt <mwitt18@my.whitworth.edu>
2016-04-07 23:51:35 -0700

    Changes made to HW 5

--- HW 5/EX05_05/EX05_05/Course.h
+++ HW 5/EX05_05/EX05_05/Course.h
@@ -5,20 +5,20 @@ using namespace std;
 
 class Course
 {
-public:
-	Course(const string& courseName, int capacity);
-	~Course();
-	string getCourseName() const;
-	void addStudent(const string& name);
-	void dropStudent(const string& name);
-	string* getStudents() const;
-	int getNumberOfStudents() const;
-
-private:
-	string courseName;
-	string* students;
-	int numberOfStudents;
-	int capacity;
+public: //Can be accessed by anyone
+    Course(const string& courseName, int capacity);
+    ~Course();
+    string getCourseName() const;
+    void addStudent(const string& name);
+    void dropStudent(const string& name);
+    string* getStudents() const;
+    int getNumberOfStudents() const;
+    
+private: //Cannot be accessed by public
+    string courseName;
+    string* students;
+    int numberOfStudents;
+    int capacity;
 };
 
 #endif
